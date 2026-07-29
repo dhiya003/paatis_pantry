218bcb81f92f11dda3f91e1fb819fb4c5c945041
dhiya003 <38076624+dhiya003@users.noreply.github.com>
2026-07-29 08:52:12 +0530

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "in.paatispantry.app",
+  appName: "Paati's Pantry",
+  webDir: "dist",
+  android: { backgroundColor: "#fbf7ef" }
+};
+
+export default config;
